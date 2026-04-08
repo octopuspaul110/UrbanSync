c703cad3cb6c2116e7e097343b18684ea4f09179
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2026-04-08 20:37:02 +0100

    feat: configure Google Sign-In URL scheme and redirect handler

--- UrbanSync/UrbanSyncApp.swift
+++ UrbanSync/UrbanSyncApp.swift
@@ -6,27 +6,19 @@
 //
 
 import SwiftUI
-import SwiftData
+import FirebaseCore
+import GoogleSignIn
 
 @main
 struct UrbanSyncApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+    
+    init() {
+        FirebaseApp.configure()
+        NotificationService.shared.requestPermission()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
