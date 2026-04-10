70574215511e22b1aa57f0e50c556b258a5d0261
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2026-04-10 09:51:16 +0100

    feat: add 5-tab layout with haptic feedback

---
+++ UrbanSync/Views/Main/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  UrbanSync
+//
+//  Created by Adegbite Paul  on 10/04/2026.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
